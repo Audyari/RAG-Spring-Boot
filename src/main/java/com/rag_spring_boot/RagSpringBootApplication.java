9eace6e7140fdfe0a2d503067890f704f7471d08
Audyari <audy123ari@gmail.com>
2026-07-10 16:13:09 +0700

    First Commit

---
+++ src/main/java/com/rag_spring_boot/RagSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.rag_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagSpringBootApplication.class, args);
+	}
+
+}
