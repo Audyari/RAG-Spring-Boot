5491912f7e8a0cf0d2d16746facbd89a45565cdc
Audyari <audy123ari@gmail.com>
2026-07-10 17:02:55 +0700

    Cek login

--- src/main/java/com/rag_spring_boot/RagSpringBootApplication.java
+++ src/main/java/com/rag_spring_boot/RagSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.rag_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.rag_spring_boot"})
 public class RagSpringBootApplication {
 
 	public static void main(String[] args) {
